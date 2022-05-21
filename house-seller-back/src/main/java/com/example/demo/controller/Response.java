5996c134d01b4ba9436e387b9ebb01decd028746
alexb997 <alex_alexandru_997@yahoo.ro>
2022-05-21 10:22:13 +0300

    small cleaning

--- house-seller-back/src/main/java/com/example/demo/controller/Response.java
+++ house-seller-back/src/main/java/com/example/demo/controller/Response.java
@@ -3,23 +3,23 @@ package com.example.demo.controller;
 import java.util.List;
 
 class Response {
-    private List items;
+    private List<?> items;
     private Integer totalPages;
     private Long totalItems;
     private Integer currentPage;
 
-    public Response(List items, Integer totalPages, Long totalItems, Integer currentPage) {
+    public Response(List<?> items, Integer totalPages, Long totalItems, Integer currentPage) {
         this.items = items;
         this.totalPages = totalPages;
         this.totalItems = totalItems;
         this.currentPage = currentPage;
     }
 
-    public List getItems() {
+    public List<?> getItems() {
         return items;
     }
 
-    public void setItems(List items) {
+    public void setItems(List<?> items) {
         this.items = items;
     }
 
