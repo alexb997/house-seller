f4a293590ba662b82489fd08385152e3daaf1149
alexb997 <alex_alexandru_997@yahoo.ro>
2022-05-21 09:53:19 +0300

    controller initialisation

--- house-seller-back/src/main/java/com/example/demo/controller/HouseController.java
+++ house-seller-back/src/main/java/com/example/demo/controller/HouseController.java
@@ -1,4 +1,17 @@
 package com.example.demo.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/houses")
 public class HouseController {
+
+    @GetMapping("/all")
+    public ResponseEntity<String> getAllHouses() {
+        return  new ResponseEntity<>("You've called house controller get all", HttpStatus.OK);
+    }
 }
