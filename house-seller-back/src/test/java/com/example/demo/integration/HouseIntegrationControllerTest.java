a720da214f692b1c04b9fe736c7342c416b4d22c
alexb997 <alex_alexandru_997@yahoo.ro>
2022-05-29 15:50:54 +0300

    integration test

--- house-seller-back/src/test/java/com/example/demo/integration/HouseIntegrationControllerTest.java
+++ house-seller-back/src/test/java/com/example/demo/integration/HouseIntegrationControllerTest.java
@@ -10,6 +10,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -37,7 +38,7 @@ public class HouseIntegrationControllerTest {
 
     @Test
     @Order(2)
-    public void createAndFindHouse() throws Exception {
+    public void createReadUpdateDeleteHouseTest() throws Exception {
         String exampleHouseJson ="{\"number\":\"121\",\"status\":\"Fully-mobilated\",\"dimensions\":\"300x600x900\",\"address\":\"Somewhere\",\"owner\":\"Ms.Nobody\",\"price\":\"1200\"}";
 
         MvcResult result = mvc.perform(post("/houses/add")
@@ -48,7 +49,26 @@ public class HouseIntegrationControllerTest {
                         .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
                 .andReturn();
 
-//        String returnedHouseID= result.getResponse().getContentAsString()
+        String returnedHouseID= result.getResponse().getContentAsString().substring(7,31);
+
+        mvc.perform(get("/houses/"+returnedHouseID).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.number",is(121)));
+
+        String exampleUpdateHouseJson ="{\"number\":\"122\",\"status\":\"Fully-mobilated\",\"dimensions\":\"300x600x900\",\"address\":\"Somewhere\",\"owner\":\"Ms.Nobody\",\"price\":\"1200\"}";
+        mvc.perform(put("/houses/edit/"+returnedHouseID)
+                        .accept(MediaType.APPLICATION_JSON).content(exampleUpdateHouseJson)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content()
+                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.number",is(122)))
+                .andDo(print());
+
+        mvc.perform(delete("/houses/remove/"+returnedHouseID)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
     }
 
 }
